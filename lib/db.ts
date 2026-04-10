88c0f8f4ab4e9e066652d8238959487d06657dcc
Keisuke Miura <193408231+ksukemiura@users.noreply.github.com>
2026-04-10 22:01:03 +0900

    Create Prisma client

--- lib/db.ts
+++ lib/db.ts
@@ -1,9 +1,9 @@
-import "server-only"
+import "dotenv/config";
+import { PrismaClient } from "@/lib/generated/prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
 
-import { neon } from "@neondatabase/serverless";
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+})
 
-const databaseUrl = process.env.DATABASE_URL;
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set.");
-}
-export const sql = neon(databaseUrl);
+export const prisma = new PrismaClient({ adapter })
